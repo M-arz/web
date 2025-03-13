70397012813ea45265522012ef75503db601909e
M-arz <166677277+M-arz@users.noreply.github.com>
2025-03-12 22:04:17 -0500

    Add files via upload

---
+++ scritps.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Bienvenido al proyecto de Sara");
+    document.getElementById("mensajeEspecial").addEventListener("click", function() {
+        alert("Sara, eres la farmacéutica más linda y especial ❤️");
+    });
+});
